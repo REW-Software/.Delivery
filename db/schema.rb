749b136460b492d07b698cf30c62a702704bf326
José Rômulo <joseromulo.10@hotmail.com>
2021-06-09 16:37:21 -0300

    Adiciona model User

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_06_09_182049) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.date "birth"
+    t.string "email"
+    t.string "cpf"
+    t.string "rg"
+    t.string "street"
+    t.string "number"
+    t.string "tipo"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
