c86fd96e43e8cfc80424d1002a035bd0409a7628
Ezequias Silva <ezequiasmoises@gmail.com>
2021-06-10 20:02:56 -0300

    Corrige formatação da validação do modelo Product.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
-    validates: name, presence: true
-    validates: price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+    validates :name, presence: true
+    validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
