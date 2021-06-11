cc2dedf17f0fd49b59d4b806501ac19c9f43f88c
WevertonCintra <wevertoncintra@outlook.com>
2021-06-11 12:56:00 -0300

    adiciona os testes do Sale

---
+++ test/controllers/sales_controller_test.rb
@@ -0,0 +1,35 @@
+require "test_helper"
+
+class SalesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @sale = sales(:one)
+  end
+
+  test "should get index" do
+    get sales_url
+    assert_response :success
+  end
+
+  test "should show sale" do
+    get sales_url(@sale)
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_sales_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_sale_url(@sale)
+    assert_response :success
+  end
+
+  test "should destroy sale" do
+    assert_difference('Sale.count', -1) do
+      delete sale_url(@sale)
+    end
+
+    assert_redirected_to sales_url
+  end
+end
