3ba20fd2099b8849d6820f729ae5911dc6a2879d
José Rômulo <joseromulo.10@hotmail.com>
2021-06-13 12:09:04 -0300

    Adiciona validates User

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ ruby '2.7.3'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.3', '>= 6.1.3.2'
 
+gem 'validates_timeliness', '~> 4.0'
 # Use Puma as the app server
 gem 'puma', '~> 5.0'
 # Use SCSS for stylesheets
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,21 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :birth, presence: true
-  validates :email, presence: true
-  validates :cpf, presence: true
-  validates :rg, presence: true
-  validates :street, presence: true
-  validates :number, presence: true
+
+  validates :name, presence: true, length: {minimum: 4, maximum: 100}
+
+  validates_date :birth, :before => lambda{Date.current}, presence:true
+
+  VALID_EMAIL_FORMAT= /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  validates :email, presence: true, length: {maximum: 150}, format: { with: VALID_EMAIL_FORMAT}
+
+  VALID_CPF_FORMAT = /[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}/
+  validates :cpf, presence:true, length: {maximum: 14}, format: {with: VALID_CPF_FORMAT}
+
+  VALID_RG_FORMAT = /[0-9]/
+  validates :rg, presence: true, length: {maximum: 14}, format: {with: VALID_RG_FORMAT}
+
+  validates :street, presence: true, length: {minimum: 4, maximum: 100}
+
+  validates :number, presence: true, length: {minimum: 1, maximum: 10}
+
   validates :tipo, presence: true
 end
