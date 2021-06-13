80ea28cf98239d029f788974ec47ed51a5dc4cff
José Rômulo <joseromulo.10@hotmail.com>
2021-06-13 12:24:23 -0300

    Corrigi erros Codacy em model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,15 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: {minimum: 4, maximum: 100}
 
-  validates_date :birth, :before => lambda{Date.current}, presence:true
+  validates_date :birth, before: -> { Date.current }, presence:true
 
-  VALID_EMAIL_FORMAT= /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  VALID_EMAIL_FORMAT = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i.freeze
   validates :email, presence: true, length: {maximum: 150}, format: { with: VALID_EMAIL_FORMAT}
 
-  VALID_CPF_FORMAT = /[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}/
+  VALID_CPF_FORMAT = /[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}/.freeze
   validates :cpf, presence:true, length: {maximum: 14}, format: {with: VALID_CPF_FORMAT}
 
-  VALID_RG_FORMAT = /[0-9]/
+  VALID_RG_FORMAT = /[0-9]/.freeze
   validates :rg, presence: true, length: {maximum: 14}, format: {with: VALID_RG_FORMAT}
 
   validates :street, presence: true, length: {minimum: 4, maximum: 100}
