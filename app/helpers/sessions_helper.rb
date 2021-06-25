f7e1be71091c67216ccecca0fc42edb3ef26f1ab
José Rômulo <joseromulo.10@hotmail.com>
2021-06-25 17:36:36 -0300

    Adiciona sessão

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,30 @@
+module SessionsHelper
+
+  def sign_in(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def block_access
+    if current_user.present?
+      redirect_to @current_user
+    end
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def sign_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  def isAdmin?
+    @current_user.tipo == 'Administrador'
+  end
+
+end
