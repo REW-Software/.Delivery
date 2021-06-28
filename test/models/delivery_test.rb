3d1e4e583c3e3c3189c96d7cf5b32d05f7f13b27
WevertonCintra <wevertoncintra@outlook.com>
2021-06-28 17:14:24 -0300

    modificação nos testes de delivery para corrigir erro no travis

--- test/models/delivery_test.rb
+++ test/models/delivery_test.rb
@@ -2,38 +2,17 @@ require "test_helper"
 
 class DeliveryTest < ActiveSupport::TestCase
   test "criar entrega" do
-    product = Product.new(name: "produto", price: 10.00)
-    product.save
-    sale = Sale.new(product_id: product.id, payment_type: "money", quantity_product: 1, name_client: "cliente", phone_client: "11999991111", street: "rua principal", number: "01")
-    sale.save
-    user = User.new(name:'Romulo', birth:'05/12/2000', email:'joseromulo@hotmail.com', cpf:'118.129.724-90', rg:'555555', street:'Manoel Braga', number:'162', tipo:'DeliveryMan', password:'123456', password_confirmation:'123456')
-    user.save
-
-    delivery = Delivery.new(sale_id: sale.id, user_id: user.id, status: "progress")
+    delivery = Delivery.new(sale_id: "1", user_id: "1", status: "progress")
     assert delivery.save
   end
 
   test "criar entrega com campos invalidos" do
-    product = Product.new(name: "produto", price: 10.00)
-    product.save
-    sale = Sale.new(product_id: product.id, payment_type: "money", quantity_product: 1, name_client: "cliente", phone_client: "11999991111", street: "rua principal", number: "01")
-    sale.save
-    user = User.new(name:'Romulo', birth:'05/12/2000', email:'joseromulo@hotmail.com', cpf:'118.129.724-90', rg:'555555', street:'Manoel Braga', number:'162', tipo:'DeliveryMan', password:'123456', password_confirmation:'123456')
-    user.save
-
-    delivery = Delivery.new(sale_id: sale.id, user_id: "", status: "")
+    delivery = Delivery.new(sale_id: "1", user_id: "", status: "")
     assert_not delivery.save
   end
 
   test "criar entrega sem venda" do
-    product = Product.new(name: "produto", price: 10.00)
-    product.save
-    sale = Sale.new(product_id: product.id, payment_type: "money", quantity_product: 1, name_client: "cliente", phone_client: "11999991111", street: "rua principal", number: "01")
-    sale.save
-    user = User.new(name:'Romulo', birth:'05/12/2000', email:'joseromulo@hotmail.com', cpf:'118.129.724-90', rg:'555555', street:'Manoel Braga', number:'162', tipo:'DeliveryMan', password:'123456', password_confirmation:'123456')
-    user.save
-
-    delivery = Delivery.new(sale_id: "", user_id: user.id, status: "concluded")
+    delivery = Delivery.new(sale_id: "", user_id: "1", status: "concluded")
     assert_not delivery.save
   end
 end
