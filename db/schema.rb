01ca3d41905d083a1e0c44ab57fb513a2b776a94
WevertonCintra <wevertoncintra@outlook.com>
2021-06-28 14:17:08 -0300

    modifica model delivery e adiciona migrate delivery

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_25_181433) do
+ActiveRecord::Schema.define(version: 2021_06_28_164402) do
+
+  create_table "deliveries", force: :cascade do |t|
+    t.integer "sale_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["sale_id"], name: "index_deliveries_on_sale_id"
+    t.index ["user_id"], name: "index_deliveries_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -46,5 +55,7 @@ ActiveRecord::Schema.define(version: 2021_06_25_181433) do
     t.string "password_digest"
   end
 
+  add_foreign_key "deliveries", "sales"
+  add_foreign_key "deliveries", "users"
   add_foreign_key "sales", "products"
 end
