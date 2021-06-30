db984a5b9d981e7828627feffa9ceeec25d2d9ab
José Rômulo <joseromulo.10@hotmail.com>
2021-06-30 11:44:10 -0300

    Adiciona relacionamento em User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :posts
+
   has_secure_password
 
   validates :name, presence: true, length: { minimum: 4, maximum: 100 }
