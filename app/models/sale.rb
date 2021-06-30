50c39824e6a8cbb7c379afb5dc95e6ea25959c1e
José Rômulo <joseromulo.10@hotmail.com>
2021-06-30 18:14:40 -0300

    Corrigi views/controller/model de sales

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -5,10 +5,4 @@ class Sale < ApplicationRecord
   enum payment_type: [:money, :card]
 
   validates :name_client, presence: true, length: { minimum: 4, maximum: 100 }
-
-  VALID_TEL_FORMAT = /\A\([0-9]{2}\)[0-9]{5}\-[0-9]{4}\z/.freeze
-  validates :phone_client, presence: true, length: {maximum: 14 }, format: {with: VALID_TEL_FORMAT}
-
-  validates :street, presence: true, length: { minimum: 5, maximum: 50 }
-  validates :number, presence: true, length: { minimum: 1, maximum: 10 }
 end
