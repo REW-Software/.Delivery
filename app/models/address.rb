d36ed121bd1cab1b0f49c9f95fb46f9b7a0982af
José Rômulo <joseromulo.10@hotmail.com>
2021-07-01 19:12:12 -0300

    Adiciona validates para address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,11 @@
 class Address < ApplicationRecord
   belongs_to :user
+
+  VALID_CEP_FORMAT= /[0-9]{5}-[0-9]{3}/
+  validates :cep, presence: true, length: {maximum: 9}, format: {with: VALID_CEP_FORMAT}
+
+  validates :city, presence: true, length: {maximum: 30}
+  validates :district, presence: true, length: {maximum: 20}
+  validates :street, presence: true, length: {maximum: 50}
+  validates :number, presence: true, length: {maximum: 15}
 end
