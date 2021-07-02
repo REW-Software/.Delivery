f46ce39f533f5bac68a653a6fef098ec42aefad9
ezequias2d <ezequiasmoises@gmail.com>
2021-07-02 11:07:35 -0300

    Adiciona passos para acesso.feature

--- features/step_definitions/generic_steps.rb
+++ features/step_definitions/generic_steps.rb
@@ -2,10 +2,18 @@ When('Eu clico no {string}') do |link_name|
   click_link link_name
 end
 
+When('Eu clico no botao {string}') do |button_name|
+  click_button button_name 
+end
+
 When('Eu clico em editar') do 
   click_link 'Editar'
 end
 
 When('Eu clico em deletar') do
   click_link 'Deletar'
+end
+
+Then('Eu vejo uma mensagem de email invalido') do
+  expect(page).to have_content('endereço de e-mail não é válido')
 end
