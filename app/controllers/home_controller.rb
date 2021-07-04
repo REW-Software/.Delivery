b03c8dbbca1ea90801e349834e3f96cc01e67f1b
José Rômulo <joseromulo.10@hotmail.com>
2021-07-04 13:24:37 -0300

    Refatora Replace Nested Conditional with Guard Clauses

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,24 +1,25 @@
+# frozen_string_literal: true
+
 class HomeController < ApplicationController
   before_action :authorize
-  
+
   def index
     @user = User.find(session[:user_id])
   end
 
   def openDeliveries
-    if logged_in? and isAdmin?
-      @open_deliveries = Post.where("status = 0")
-    else
-      @open_deliveries = Post.where("user_id = ? and status = 0", session[:user_id])
-    end
+    @open_deliveries = if logged_in? and isAdmin?
+                         Post.where('status = 0')
+                       else
+                         Post.where('user_id = ? and status = 0', session[:user_id])
+                       end
   end
 
   def closedDeliveries
-    if logged_in? and isAdmin?
-      @closed_deliveries = Post.where("status = 1")
-    else
-      @closed_deliveries = Post.where("user_id = ? and status = 1", session[:user_id])
-    end
+    @closed_deliveries = if logged_in? and isAdmin?
+                           Post.where('status = 1')
+                         else
+                           Post.where('user_id = ? and status = 1', session[:user_id])
+                         end
   end
-
 end
