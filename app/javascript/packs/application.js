16496d4ecd7546afa8dba2d142d216f6fbabe3cb
José Rômulo <joseromulo.10@hotmail.com>
2021-07-04 13:38:33 -0300

    Correção erros codacy

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,12 +7,12 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap'
+import "bootstrap";
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
 Rails.start()
 Turbolinks.start()
