6682a86ca65a62d78298fb5a8ecf5f02d217b2b4
ezequias2d <ezequiasmoises@gmail.com>
2021-07-05 15:24:48 -0300

    Adiciona usuario cliente e atualiza alguns atributos de outros usuarios no populador de banco de dados

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,12 @@
 module Seeds 
   def self.run
     user = User.new(name:'admin', birth:'05/12/2000', email:'admin@admin.com', cpf:'480.412.910-31', rg:'173382885', user_type:'Administrador', password:'admin1', password_confirmation:'admin1')
-    deliveryMan = User.new(name:'deliveryMan', birth:'05/12/2000', email:'deliveryMan@deliveryMan.com', cpf:'480.412.910-31', rg:'173382885', user_type:'DeliveryMan', password:'deliveryman', password_confirmation:'deliveryman')
+    deliveryMan = User.new(name:'deliveryMan', birth:'05/12/2000', email:'deliveryMan@deliveryMan.com', cpf:'112.695.480-26', rg:'241409184', user_type:'DeliveryMan', password:'deliveryman', password_confirmation:'deliveryman')
+    client = User.new(name: 'Matheus', birth:'15/02/1995', email:'matheus@mail.com', cpf:'523.709.300-04', rg: '129911355', user_type:'Client', password:'123456', password_confirmation:'123456')
       
     user.save
     deliveryMan.save
+    client.save
   end
 end
 
